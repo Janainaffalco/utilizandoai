03b6f8ab2a884ff5afffceffc7c49e3e7834ca11
Janaina <janainaster@gmail.com>
2025-03-13 15:28:59 -0300

    Removendo arquivos .gitignore

---
+++ .expo/types/router.d.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import * as Router from 'expo-router';
+
+export * from 'expo-router';
+
+declare module 'expo-router' {
+  export namespace ExpoRouter {
+    export interface __routes<T extends string | object = string> {
+      hrefInputParams: { pathname: Router.RelativePathString, params?: Router.UnknownInputParams } | { pathname: Router.ExternalPathString, params?: Router.UnknownInputParams } | { pathname: `/`; params?: Router.UnknownInputParams; } | { pathname: `/_sitemap`; params?: Router.UnknownInputParams; } | { pathname: `/create`; params?: Router.UnknownInputParams; } | { pathname: `/nutrition`; params?: Router.UnknownInputParams; } | { pathname: `/step`; params?: Router.UnknownInputParams; };
+      hrefOutputParams: { pathname: Router.RelativePathString, params?: Router.UnknownOutputParams } | { pathname: Router.ExternalPathString, params?: Router.UnknownOutputParams } | { pathname: `/`; params?: Router.UnknownOutputParams; } | { pathname: `/_sitemap`; params?: Router.UnknownOutputParams; } | { pathname: `/create`; params?: Router.UnknownOutputParams; } | { pathname: `/nutrition`; params?: Router.UnknownOutputParams; } | { pathname: `/step`; params?: Router.UnknownOutputParams; };
+      href: Router.RelativePathString | Router.ExternalPathString | `/${`?${string}` | `#${string}` | ''}` | `/_sitemap${`?${string}` | `#${string}` | ''}` | `/create${`?${string}` | `#${string}` | ''}` | `/nutrition${`?${string}` | `#${string}` | ''}` | `/step${`?${string}` | `#${string}` | ''}` | { pathname: Router.RelativePathString, params?: Router.UnknownInputParams } | { pathname: Router.ExternalPathString, params?: Router.UnknownInputParams } | { pathname: `/`; params?: Router.UnknownInputParams; } | { pathname: `/_sitemap`; params?: Router.UnknownInputParams; } | { pathname: `/create`; params?: Router.UnknownInputParams; } | { pathname: `/nutrition`; params?: Router.UnknownInputParams; } | { pathname: `/step`; params?: Router.UnknownInputParams; };
+    }
+  }
+}
